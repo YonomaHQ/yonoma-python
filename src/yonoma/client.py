4e4f72cca01658f4b345234760032119493820c4
karthickTwinArcus <karthick@twinarcus.com>
2025-03-29 13:26:40 +0530

    Update client.py

--- src/yonoma/client.py
+++ src/yonoma/client.py
@@ -1,14 +1,17 @@
 import requests
 from yonoma.exceptions import YonomaAPIError
+from yonoma.version import VERSION
 
 class YonomaClient:
     BASE_URL = "https://api.yonoma.io/v1/"
 
     def __init__(self, api_key):
         self.api_key = api_key
+        self.version = VERSION
         self.headers = {
             "Authorization": f"Bearer {self.api_key}",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "User-Agent": f"yonoma-python: {self.version}"
         }
 
     def request(self, method, endpoint, data=None):
